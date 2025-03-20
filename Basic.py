5aabcc147fdab042dbb53667d96d5a8359bec76b
Jauwwad <jauwwad.nallamandu786@gmail.com>
2025-03-20 15:16:30 +0530

    first

---
+++ Basic.py
@@ -0,0 +1,89 @@
+# a=input("Enter a String: ")                                   # ( % ) used to find remainder
+# a=a+"\n"                                                      # ( // ) used to find Quotient
+# b=int(input("enter number of repetation"))
+# print(a*b)
+
+
+# print(type(a),type(b))
+
+
+# a=input("Enter your name: ")
+# print("Hello",a)
+
+
+# a=float(input("Enter Hours:"))
+# b=float(input("Enter Rate:"))
+# print("pay:",pay)
+# pay=(a*b)
+
+
+# a=True
+# print(type(a))
+
+
+# x=int(input("Enter vale of x:"))                      # If else
+# if x<10:
+#     print("x is smaller then 10")
+# else:
+#     print("x is greater then 10")
+
+
+# x=int(input("Enter value of X:"))                       # elif
+# y=int(input("Enter value of Y:"))
+# if x<y:
+#     print("y is greater then x")
+# elif x>y:
+#     print("x is greater then y")
+# else:
+#     print("x and y are equal")
+ 
+
+# a="abcdxyz"
+# print(max(a))
+# print(min(a))
+# print(len(a))
+
+
+# import random
+# for i in range (2):
+#      x=random.random()
+#      print(x)
+
+# print(random.randint(1,10))                          # .randint(low,high)
+
+
+# def jauwwad():
+#     print("Hello my name is jauwwad")                   # defining a function
+#     print("I am studing in orchid college")   
+# def repeat_jauwwad():
+#     print(jauwwad())
+#     print(jauwwad())
+# print(repeat_jauwwad())
+    
+
+# def add(a,b):                   # output: 5
+#     added=a+b
+#     return added                # compalsary line
+# x=add(1,4)
+# print(x)
+
+
+# friends=['jauwwad','raza','sohail','varun']
+# for friend in friends:
+#     print("hello",friend)
+# print("Done!")
+
+
+# import random
+# a=random.randint(1,4)
+# if (a==1):
+#     print("answer is 1")
+# elif(a==2):
+#     print('answer is 2')
+# elif(a==3):
+#     print('answer is 3')
+# elif(a==4):
+#     print('answer is 4')
+# else:
+#     print('ok')
+
---
+++ strings.py
@@ -0,0 +1,16 @@
+name= 'jauwwad'
+# print(name[0])
+# print(name[1])
+# print(len(name))
+
+# print(name[0:3])
+# print(name[4:6])
+
+# print(name[::-1])
+print(name[::-1])
+
+
+# for character in name:                  # For loop in strings
+#     print(character)
+# print("Done!")
+
